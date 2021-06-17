4c8a30c0471a69c329a6fd9e2c7219d4121ae9da
winnie kiara <winniekk20@gmail.com>
2021-06-17 11:00:08 -0700

    all changes saved

--- register.js
+++ register.js
@@ -4,13 +4,13 @@
 var checkNullField = () => { 
     let fullName = document.getElementById('fullname');
     let date = document.getElementById('date');
-    let dropDown = document.getElementById('dropdown');
-    let dropDown2 = document.getElementById('dropdown2');
+    let dropDownGender = document.getElementById('dropdown');
+    let dropDownVaccine = document.getElementById('dropdown2');
     let nin= document.getElementById('nin');
     let temperature = document.getElementById('temp');
     let pickup = document.getElementById('pickup');
     let dropoff= document.getElementById('destination');
-    
+    let errornin = document.getElementById('errorNIN')
     
     let nullField = document.querySelector('.null-field')
 
@@ -27,23 +27,25 @@ var checkNullField = () => {
         date.style.border = "2px solid green"; 
     }
 
-    if(dropDown.value == "select your Gender") {
-        dropDown.style.border = "2px solid red";
+    if(dropDownGender.value == "select your Gender") {
+        dropDownGender.style.border = "2px solid red";
     }else{
- dropDown.style.border = "2px solid green";
+ dropDownGender.style.border = "2px solid green";
     }
 
-     if(dropDown2.value == "Vaccination status"){
-       dropDown2.style.border = "2px solid red";
+     if(dropDownVaccine.value == "Vaccination status"){
+       dropDownVaccine.style.border = "2px solid red";
     } else{
-        dropDown2.style.border = "2px solid green";
+        dropDownVaccine.style.border = "2px solid green";
     }
 
     if(nin.value == "" || nin.value.length !=14){
         nin.style.border = "2px solid red";
-        alert('NIN must be 14 numbers!')
+       errornin.innerHTML = 'NIN must have 14 characters'
+       errornin.style = 'color:white; font-size:14px;'
     } else{
         nin.style.border = "2px solid green"; 
+        errornin.innerHTML = ''
     }
 
     if(temperature.value == "") {
